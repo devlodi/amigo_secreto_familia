1bcdf45901573229b1cca82f74e8e00a2e7d4213
devlodi <143453595+devlodi@users.noreply.github.com>
2023-12-18 21:36:10 -0300

    Update your-script.js

--- your-script.js
+++ your-script.js
@@ -1,4 +1,5 @@
-// A configuração do Firebase. Isso é seguro para ser exposto publicamente conforme a documentação do Firebase.
+console.log("Carregando configuração do Firebase.");
+
 const firebaseConfig = {
   apiKey: "AIzaSyDXOqm52mwBr4RH3KuP5MrGeyYg1qQy27M",
   authDomain: "amigo-secreto-8b3ae.firebaseapp.com",
@@ -9,29 +10,35 @@ const firebaseConfig = {
   appId: "1:133611460498:web:2641aafc8c318aaa4d134b"
 };
 
-// Inicialize o Firebase
+console.log("Inicializando o Firebase.");
 firebase.initializeApp(firebaseConfig);
+console.log("Firebase inicializado.");
 
-// Referência ao banco de dados
 const database = firebase.database();
 
 function submitName() {
+    console.log("submitName chamado.");
     const nameInput = document.getElementById('nameInput');
     const name = nameInput.value;
     const dateTime = new Date().toISOString();
 
-    // Insira o nome no banco de dados Firebase
+    console.log(`Adicionando nome: ${name}, Data/Hora: ${dateTime}`);
     firebase.database().ref('participants').push().set({
         nome: name,
         data_hora: dateTime
+    }, (error) => {
+        if (error) {
+            console.error("Erro ao adicionar participante:", error);
+        } else {
+            console.log("Participante adicionado com sucesso.");
+        }
     });
 }
 
-// Escute por novos participantes e atualize a tabela
 firebase.database().ref('participants').on('value', (snapshot) => {
+    console.log("Dados recebidos do Firebase.", snapshot.val());
     const participantsTable = document.getElementById('participantsTable');
     
-    // Limpe a tabela antes de inserir os novos valores
     participantsTable.innerHTML = `
         <tr>
             <th>Nome</th>
@@ -41,8 +48,6 @@ firebase.database().ref('participants').on('value', (snapshot) => {
     
     snapshot.forEach((childSnapshot) => {
         const childData = childSnapshot.val();
-        
-        // Crie uma nova linha na tabela para cada participante
         const row = participantsTable.insertRow(-1);
         const cellName = row.insertCell(0);
         const cellDate = row.insertCell(1);
